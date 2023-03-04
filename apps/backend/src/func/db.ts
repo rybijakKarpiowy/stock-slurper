0b5a24990bacf95872724fa947dcf174135caa1f
Antoni Jurga <antekjurga@gmail.com>
2023-03-04 17:32:41 +0100

    Unified db tables and db fetches

--- apps/backend/src/func/db.ts
+++ apps/backend/src/func/db.ts
@@ -2,26 +2,30 @@ import { PrismaClient } from "@prisma/client";
 import { Product } from "./scrapers/par";
 const prisma = new PrismaClient();
 
-export const saveToDB = async (data: Product[], table: "Asgard" | "Par" | "Axpol") => {
-    console.log(`Saving to ${table}...`)
-    switch (table) {
-        case "Asgard":
-            await prisma.asgard.createMany({
-                data,
-            });
-            console.log("Asgard saved to db")
-            break
-        case "Par":
-            // await prisma.par.createMany({
-            //     data,
-            // });
-            console.log("Par saved to db")
-            break
-        case "Axpol":
-            // await prisma.axpol.createMany({
-            //     data,
-            // });
-            console.log("Axpol saved to db")
-            break
-    }
+export const saveToDB = async (data: Product[], company: "Asgard" | "Par" | "Axpol") => {
+    console.log(`Saving to ${company}...`);
+    const codesDB = await prisma.items.findMany({
+        select: {
+            code: true,
+        },
+    });
+
+    const itemsNotIncluded = data.filter((item) => !codesDB.includes({ code: item.code }));
+    await prisma.items.createMany({
+        data: itemsNotIncluded.map((item) => ({
+            code: item.code,
+            name: item.name,
+            link: item.link,
+            company,
+        })),
+    });
+
+    await prisma.stock.createMany({
+        data: data.map((item) => ({
+            code: item.code,
+            amount: item.amount,
+            price: item.price,
+            company,
+        })),
+    });
 };
