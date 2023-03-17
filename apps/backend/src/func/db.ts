6639835d27395558078f34e57e9f8ab5b6aae577
Antoni Jurga <antekjurga@gmail.com>
2023-03-17 12:48:38 +0100

    Reaplly disguise

--- apps/backend/src/func/db.ts
+++ apps/backend/src/func/db.ts
@@ -152,7 +152,18 @@ export const getNDaysOfCompany = async (
         return item;
     });
 
-    return itemsHistoryArraySorted;
+    const itemHistoryArrayDisguised = itemsHistoryArraySorted.map((itemsHistory: ItemHistory) => {
+        const disguise = Math.random() * parseFloat(process.env.DISGUISE_DELTA as string) + parseFloat(process.env.DISGUISE_BASE as string);
+        return {
+            ...itemsHistory,
+            history: itemsHistory.history.map((history) => ({
+                ...history,
+                price: history.price * disguise,
+            })),
+        };
+    });
+
+    return itemHistoryArrayDisguised;
 };
 
 export const maxDays = async (n: number, itemIds: number[]) => {
