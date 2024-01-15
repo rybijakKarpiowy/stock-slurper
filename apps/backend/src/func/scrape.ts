8ca58aed6d33ce22d4afe29c1415716975f5a70a
Antoni Jurga <antekjurga@gmail.com>
2024-01-15 21:45:07 +0100

    Memory leak debug logs

--- apps/backend/src/func/scrape.ts
+++ apps/backend/src/func/scrape.ts
@@ -9,7 +9,7 @@ import { mobScraper } from "./scrapers/mob";
 
 export const scrape = async (company: companyName) => {
     let data: Product[] = [];
-    console.log(`Scraping ${company}...`)
+    console.log(`Scraping ${company}...`);
     switch (company) {
         case "Asgard":
             data = (await asgardScraper().catch((err) => console.log(err))) || [];
@@ -29,10 +29,18 @@ export const scrape = async (company: companyName) => {
         case "MOB":
             data = (await mobScraper().catch((err) => console.log(err))) || [];
             break;
+        // case "PfConcept":
+        //     data = (await pfconceptScraper().catch((err) => console.log(err))) || [];
+        //     break;
     }
 
     if (data.length === 0) {
         console.log(`${company} scraper failed`);
+
+        // look for memory leaks
+        const memoryUsage = process.memoryUsage();
+        console.log(`Memory usage after ${company} scrape:`, memoryUsage);
+
         return;
     }
 
@@ -41,4 +49,10 @@ export const scrape = async (company: companyName) => {
     await saveToDB(data, company)
         .then(() => console.log(`${company} saved to db`))
         .catch((err) => console.log(err));
+
+    // look for memory leaks
+    const memoryUsage = process.memoryUsage();
+    console.log(`Memory usage after ${company} scrape:`, memoryUsage);
+
+    return;
 };
